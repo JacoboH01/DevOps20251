59f064c5596f1802d019d870805ce992f0126c5b
jacoboHr7 <jherrera@ieo.com.co>
2025-04-27 22:51:16 -0500

    fix: organizo la conexion para que no me la rechace rillway

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,12 +4,8 @@ require('dotenv').config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // Necesario para Neon
+    rejectUnauthorized: false // Necesario para Railway o Neon
   }
 });
-
-pool.connect()
-  .then(() => console.log('✅ Conexión a la base de datos establecida'))
-  .catch(err => console.error('❌ Error conectando a la base de datos:', err));
 
 module.exports = pool;
